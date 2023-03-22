a15f9dfe1c0f701df5f685aa8ce2546572ab4bdd
raphael-malet <115145341+raphael-malet@users.noreply.github.com>
2023-03-22 12:36:02 +0100

    push chat_page
    ajout liste afficher les message 
    probleme pour afficher les message recus

--- chat_page.py
+++ chat_page.py
@@ -7,8 +7,10 @@ import socket
 import threading
 from datetime import datetime
 from datetime import date
+import tkinter.scrolledtext
+import time
 
-admin = mysql.connector.connect(host="localhost", user="root", password="ClemsSQL!13", database="mydiscord")
+admin = mysql.connector.connect(host="localhost", user="root", password="vatefaireencule", database="mydiscord")
 cursor = admin.cursor()
 
 class Chat_page:
@@ -17,13 +19,16 @@ class Chat_page:
         self.email = email
         self.continuer = False
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.liste_message = []
+        self.longueur_liste = 0
+        self.dernier_message = ''
 
         fenetre = tk.Tk()
-        fenetre.geometry('600x700')
+        fenetre.geometry('600x720')
         fenetre.title('My Discord')
         fenetre.configure(bg='#000000')
         try:
-            self.client.connect(('10.10.3.160', 1025))
+            self.client.connect(('10.10.6.22', 1025))
         except:
             tk.messagebox.showwarning("Test","Test")
 
@@ -37,14 +42,24 @@ class Chat_page:
         liste_channel = Listbox(section_chanel_gauche, width=10, fg='BLACK', bg='RED', height=39)
         liste_channel.grid(pady=5)
 
+
+
         section_droite = Frame()
         section_droite.configure(bg='WHITE', width=500)
         section_droite.pack(side=RIGHT, fill=Y)
 
+        section_droite_afficher_message = Frame(section_droite)
+        section_droite_afficher_message.grid(rows=1)
+
+        self.section_afficher_message =tkinter.scrolledtext.ScrolledText(section_droite, bg='GREY', width=50)
+        self.section_afficher_message.grid(sticky=N)
+
+
         # Section Chat
         section_entrer_chat = Frame(section_droite)
         section_entrer_chat.configure(bg='GREY', width=500, height=100)
-        section_entrer_chat.pack(side=BOTTOM, fill=X)
+        section_entrer_chat.grid(row=2, )
+
 
         self.section_texte_chat = Entry(section_entrer_chat, bg='WHITE', width=40, fg='BLACK')
         self.section_texte_chat.grid(column=4,row=2)
@@ -52,19 +67,21 @@ class Chat_page:
         bouton_entrer_chat = Button(section_entrer_chat, text='Entrer',command=lambda : self.EnvoieMessage(self.section_texte_chat.get()))
         bouton_entrer_chat.grid(column=2,row=2)
 
-
-
         afficher_fenetre = threading.Thread(target=self.lancerfenetre, args=fenetre)
         afficher_fenetre.start()
 
         recois_message = threading.Thread(target=self.recoisMessage)
         recois_message.start()
 
-        #envoie_message = threading.Thread(target=self.EnvoieMessage)
-        #envoie_message.start()
+        #afficher_message_recus = threading.Thread(target=self.afficher_message_dans_listebox)
+        #afficher_message_recus.start()
+
+        envoie_message = threading.Thread(target=self.EnvoieMessage)
+        envoie_message.start()
 
     def lancerfenetre(self, fenetre):
         fenetre = fenetre
+        self.afficher_message_dans_listebox()
         fenetre.mainloop()
 
     def pseudoUtilisateur(self):
@@ -80,19 +97,25 @@ class Chat_page:
 
     def recoisMessage(self):
         self.pseudo = self.pseudoUtilisateur()
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect(('10.10.3.160', 1025))
         while True:
-            try:
-                message = self.client.recv(1024).decode('ascii')
-                if message == 'NICK':
-                    self.client.send(self.pseudo.encode('ascii'))
-                else:
-                    print(message)
-            except:
-                print('error occured!')
-                self.client.close()
-                break
+            #try:
+            self.message = self.client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                self.client.send(self.pseudo.encode('utf-8'))
+            else:
+                try:
+                    if bool(self.message):
+                        self.liste_message.append(message)
+                        print(self.liste_message)
+                        self.dernier_message = message
+                        print(self.dernier_message)
+                        self.section_afficher_message.insert(END, self.message)
+                except:
+                    pass
+            #except:
+             #   print('error occured!')
+              #  self.client.close()
+               # break
 
     def EnvoieMessage(self, mess):
         heure = datetime.now()
@@ -101,7 +124,24 @@ class Chat_page:
         jour = aujourdhui.strftime("%d/%m/%Y")
         self.pseudo = self.pseudoUtilisateur()
         message = f'[{jour}] [{heure_actuel}] {self.pseudo}: {mess}'
-        self.client.send(message.encode('ascii'))
+        self.client.send(message.encode('utf-8'))
+        self.section_afficher_message.insert(END, message)
+        self.section_texte_chat.delete(0, END)
+
+
+    def afficher_message_dans_listebox(self):
+        if self.longueur_liste != len(self.liste_message):
+            mess = self.dernier_message
+            print(type(mess))
+            self.section_afficher_message.insert(END, mess)
+            self.longueur_liste = len(self.liste_message)
+
+
+
+
+
+
+
 
 
 
