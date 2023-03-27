919cc21b06243a76ac09b61a647648cc99cfd0f9
raphael-malet <115145341+raphael-malet@users.noreply.github.com>
2023-03-27 11:05:22 +0200

    fix bug update du chat
    ajout de l'affichage instanté du chat + mise a jour de la database

--- chat_page.py
+++ chat_page.py
@@ -15,19 +15,24 @@ class Chat_page:
         self.email = email
         self.continuer = False
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(('10.10.1.143', 1025))
         self.liste_message = []
         self.longueur_liste = 0
         self.dernier_message = ''
+        self.gui_done = False
+        self.running = True
+        affichage = threading.Thread(target=self.Fenetreloop)
+        affichage.start()
+        recois_message = threading.Thread(target=self.receive)
+        recois_message.start()
 
 
+    def Fenetreloop(self):
         fenetre = tk.Tk()
         fenetre.geometry('640x720')
         fenetre.title('My Discord')
         fenetre.configure(bg='#000000')
-        try:
-            self.client.connect(('10.10.6.22', 1025))
-        except:
-            print("Erreur")
+        fenetre.resizable(False,False)
 
         section_chanel_gauche = Frame()
         section_chanel_gauche.configure(bg='WHITE')
@@ -39,8 +44,6 @@ class Chat_page:
         liste_channel = Listbox(section_chanel_gauche, width=10, fg='BLACK', bg='RED', height=39)
         liste_channel.grid(pady=5)
 
-
-
         section_droite = Frame()
         section_droite.configure(bg='WHITE', width=500)
         section_droite.pack(side=RIGHT, fill=Y)
@@ -48,28 +51,28 @@ class Chat_page:
         section_droite_afficher_message = Frame(section_droite)
         section_droite_afficher_message.grid(rows=1)
 
-        self.section_afficher_message =tkinter.scrolledtext.ScrolledText(section_droite, bg='GREY', width=50)
+        self.section_afficher_message = tkinter.scrolledtext.ScrolledText(section_droite, bg='GREY', width=50, height=34)
         self.section_afficher_message.grid(sticky=N)
-
 
         # Section Chat
         section_entrer_chat = Frame(section_droite)
         section_entrer_chat.configure(bg='GREY', width=500, height=100)
         section_entrer_chat.grid(row=2, )
 
-
         self.section_texte_chat = Entry(section_entrer_chat, bg='WHITE', width=40, fg='BLACK')
-        self.section_texte_chat.grid(column=4,row=2)
+        self.section_texte_chat.grid(column=4, row=2)
 
-        bouton_entrer_chat = Button(section_entrer_chat, text='Entrer',command=lambda : self.EnvoieMessage(self.section_texte_chat.get()))
-        bouton_entrer_chat.grid(column=2,row=2)
+        bouton_entrer_chat = Button(section_entrer_chat, text='Entrer',
+                                    command=lambda: self.EnvoieMessage(self.section_texte_chat.get()))
+        bouton_entrer_chat.grid(column=2, row=2)
+        commande = "select message from public"
+        cursor.execute(commande)
+        cursor.fetchall()
+        for message in cursor:
+            self.section_afficher_message.insert('end',str(message))
 
-        bouton_actu = Button(section_entrer_chat, text='Entrer',command=lambda : self.actu())
-        bouton_actu.grid(column=2,row=3)
-
-
-        recois_message = threading.Thread(target=self.recoisMessage)
-        recois_message.start()
+        self.gui_done=True
+        fenetre.mainloop()
 
 
     def pseudoUtilisateur(self):
@@ -83,20 +86,26 @@ class Chat_page:
                 pseudo = x + ' '+i[1]
                 return pseudo
 
-    def recoisMessage(self):
+    def receive(self):
         self.pseudo = self.pseudoUtilisateur()
-        while True:
-            self.message = self.client.recv(1024).decode('utf-8')
-            if self.message == 'NICK':
-                self.client.send(self.pseudo.encode('utf-8'))
-            else:
-                try:
-                    if bool(self.message):
-                        self.liste_message.append(self.message)
+        while self.running:
+            try:
+                self.message = self.client.recv(1024).decode('utf-8')
+                if self.message == 'NICK':
+                    self.client.send(self.pseudo.encode('utf-8'))
+                else:
+                    if self.gui_done:
                         print(self.message)
-                        self.section_afficher_message.insert(END,self.message)
-                except:
-                    pass
+                        self.section_afficher_message.config(state='normal')
+                        self.section_afficher_message.insert('end', self.message)
+                        self.section_afficher_message.yview('end')
+                        self.section_afficher_message.config(state='disabled')
+            except ConnectionAbortedError:
+                break
+            except:
+                print('error')
+                self.client.close()
+                break
 
     def EnvoieMessage(self, mess):
         heure = datetime.now()
@@ -108,14 +117,7 @@ class Chat_page:
         self.client.send(message.encode('utf-8'))
         self.section_afficher_message.insert(END, message)
         self.section_texte_chat.delete(0, END)
-
-    def actu(self):
-        self.section_afficher_message.delete("1.0",END)
-        for i in self.liste_message:
-            self.section_afficher_message.insert(END,i+"\n")
-
-
-
-
-
-
+        valeur = message
+        commande = "insert into public (message) values (%s)"
+        cursor.execute(commande, valeur)
+        admin.commit()
--- myDiscord.sql
+++ myDiscord.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.31, for macos13.0 (arm64)
 --
 -- Host: localhost    Database: mydiscord
 -- ------------------------------------------------------
@@ -16,6 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `public`
+--
+
+DROP TABLE IF EXISTS `public`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `public` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `message` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `public`
+--
+
+LOCK TABLES `public` WRITE;
+/*!40000 ALTER TABLE `public` DISABLE KEYS */;
+/*!40000 ALTER TABLE `public` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `utilisateurs`
 --
 
@@ -31,7 +54,7 @@ CREATE TABLE `utilisateurs` (
   `id_channel` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `mail` (`mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +63,7 @@ CREATE TABLE `utilisateurs` (
 
 LOCK TABLES `utilisateurs` WRITE;
 /*!40000 ALTER TABLE `utilisateurs` DISABLE KEYS */;
-INSERT INTO `utilisateurs` VALUES (1,'{nom}','{prenom}','{mail}','{mdp}',NULL),(3,'{self.nom}','{self.prenom}','{self.mail}','{self.mdp}',NULL),(7,'DE','dede','deded','dede',NULL),(8,'Abdul','Gam','abdul@gmail.com','123',NULL),(10,'te','te','te','te',NULL),(12,'aze','aze','aze','aze',NULL),(15,'as','azam','poede','20ece1353d8c23fac54363b0729f63cfbf349037856da286a37e1570b7dcd9c0',NULL),(16,'felpfel','feflekof','eofkeofkzk','5fbd73326471aab714ff3bd944c5095ef89da9e5ebd3caef9300f83005e585a7',NULL),(17,'test','test','test','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08',NULL),(18,'Abdul','Gaming','abab','a667282675f4876021d392aa6592f39dabf718748c4b738563cb9d5dc8f21f24',NULL),(20,'Aziz','Ziz','ziz@mail.fr','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3',NULL),(22,'Aziz','Farouk','farouk@mail.fr','863bba74ea78b2b913311420656dedf7603a07ff65ebf0aeb9f15bf27a698838',NULL),(23,'Fab','Rice','rice@mail.fr','1b9bc7b7376e6c5c0f1042dfdd42ac68017d8cee72988967d7c670d40c466c85',NULL),(26,'Paul','Polo','polo@gmail.com','e48d077f523063e4b5652e7a5b2e01eb1fc68f3c4adabcc17d5ec5207ad4536f',NULL),(30,'pipe','pope','pipepope@mail.fr','2a3108c4e5af4b0bbca73901ab25587038583d5161d52f1f4a48998820a5d129',NULL);
+INSERT INTO `utilisateurs` VALUES (1,'{nom}','{prenom}','{mail}','{mdp}',NULL),(3,'{self.nom}','{self.prenom}','{self.mail}','{self.mdp}',NULL),(7,'DE','dede','deded','dede',NULL),(8,'Abdul','Gam','abdul@gmail.com','123',NULL),(10,'te','te','te','te',NULL),(12,'aze','aze','aze','aze',NULL),(15,'as','azam','poede','20ece1353d8c23fac54363b0729f63cfbf349037856da286a37e1570b7dcd9c0',NULL),(16,'felpfel','feflekof','eofkeofkzk','5fbd73326471aab714ff3bd944c5095ef89da9e5ebd3caef9300f83005e585a7',NULL),(17,'test','test','test','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08',NULL),(18,'Abdul','Gaming','abab','a667282675f4876021d392aa6592f39dabf718748c4b738563cb9d5dc8f21f24',NULL),(20,'Aziz','Ziz','ziz@mail.fr','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3',NULL),(22,'Aziz','Farouk','farouk@mail.fr','863bba74ea78b2b913311420656dedf7603a07ff65ebf0aeb9f15bf27a698838',NULL),(23,'Fab','Rice','rice@mail.fr','1b9bc7b7376e6c5c0f1042dfdd42ac68017d8cee72988967d7c670d40c466c85',NULL),(26,'Paul','Polo','polo@gmail.com','e48d077f523063e4b5652e7a5b2e01eb1fc68f3c4adabcc17d5ec5207ad4536f',NULL),(30,'pipe','pope','pipepope@mail.fr','2a3108c4e5af4b0bbca73901ab25587038583d5161d52f1f4a48998820a5d129',NULL),(33,'adlakdnad','daniodaldhad','dandioadk@mail.fr','4aff55d4115123290658e9c0b474a84837475080e10457d0c69ba97274d79667',NULL),(34,'raphael','malet','raph@mail.fr','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3',NULL),(35,'pierre','pierre','pierre.pierre@pierre.pierre','pierre',NULL),(36,'lneknr','maletdakl ','rah@mail.fr','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3',NULL),(37,'dnakld','d,nakld','ddalkd@mail.fr','6fe8ecbc1deafa51c2ecf088cf364eba1ceba9032ffbe2621e771b90ea93153d',NULL);
 /*!40000 ALTER TABLE `utilisateurs` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-21 12:31:31
+-- Dump completed on 2023-03-27 11:02:58
